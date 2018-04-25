0ece34eeea96f56b6e094e5d122eb8f7c05552e9
Anthony Hills <ah00446@surrey.ac.uk>
2018-04-25 10:37:36 +0100

    updated statistical_arbitrage.f90

--- statistical_arbitrage.f90
+++ statistical_arbitrage.f90
@@ -8,102 +8,6 @@
 ! We use the Black-Scholes model to simulate the share price over time. ...
 !=============================================================================
 PROGRAM statistical_arbitrage
-  ! Parameters
-  INTEGER, PARAMETER :: t_invest_bond = 100  ! Time sell for risk free bond
-  INTEGER, PARAMETER :: t0 = 0, dt = 1  ! Final day, Integration time
-  REAL, PARAMETER :: r =   ! Risk free rate
-
-
-  INTEGER :: t
-  REAL :: r, share_price, k
-  REAL, DIMENSION(t0:t_invest_bond) :: v, share_price, discounted_value
-
-
-!================================================================
-! (a) Derive the present value of the portfolio given in Eq. (2)
-!================================================================
-
-share_price(1) = 100  ! Share price at t = 0
-t_invest_bond = 100
-!
-
-! Portfolio discounted value as a function of time (Eq. 2)
-IF t <= t_invest_bond:
-  v(t) = share_price(t) * exp(-r*t) - share_price(1)
-ELSE:
-  v(t) = share_price(1) * k
-END IF
-
-! Equation 2:
-dt = 1
-discounted_value
-discounted_value(t)
-
-!===========================================================================
-! (b) Function that generates a normally distributed random number with
-! mean zero and variance one from the Box-Muller transform
-!===========================================================================
-FUNCTION rand_box_muller(seed1, seed2):
-  ! Intrinsic uniform random number
-  x1 = rand(seed1)
-  x2 = rand(seed2)
-
-  ! Generate normally distributed numbers, according to Box-Muller transform
-  z1 = sqrt(-2.0 * ln(x1) * cos(2.0 * pi * x2))
-  z2 = sqrt(-2.0 * ln(x1)) * sin (2.0 * pi * x2)
-
-  ! Only return either z1 or z2
-  RETURN z1, z2
-END FUNCTION
-
-! Perhaps visualize lots of random numbers drawn from this function for report
-
-
-!==============================================================================
-! (b) Produce a realisation of daily share price for an annual drift, mu = 0.16
-! , and volatility, sigma = 0.2. Assume 250 trading days in a year. Use this to
-! simulate daily share price for one year.
-!==============================================================================
-! Parameters
-mu = 0.16  ! annual drift
-sigma = 0.2  ! volatility
-t_final = 250  ! trading days in a year
-dt = 1  ! daily intervals
-share_price(1) = 100  ! Starting share price at t=0
-dw = ...  ! Wiener process
-
-! Simulate daily share price over one year
-t = 1
-while t <= t_final:
-  delta_share = (mu * dt + sigma * dw) * share_price(t)
-  t += dt
-  share_price(t) = share_price(t-1) + delta_share  ! Evaluate tomorrow's price
-
-share_price_after(1, years, realisations)
-
-
-r = 0.04  ! risk-free rate
-k = 0.2  !
-
-
-realisations = 1000
-dt = realisations / t_final
-
-t_final = 1 * 250  ! 1 year
-t_final = 2 * 250  ! 2 years
-t_final = 5 * 250  ! 5 years
-
-! Evaluate v from t=0 up to 20 years
-years = 20
-mean_v = SUM(v) / realisations
-the_sum = 0
-n = realisations
-
-FOR i=0, n:
-  the_sum = the_sum (v(i) - mean_v) ** 2.
-  variance_v = 1.0/(n-1) * sum(x(i) - mean_v) ** 2
-END FOR
-
 
 WRITE(6,*) "Answers to the coursework:"
 WRITE(6,*) "(a)", present_value, " = present value of portfolio "
@@ -124,4 +28,111 @@ WRITE(6,*) "                       probability_of_loss.dat"
 WRITE(6,*) ""
 WRITE(6,*) "(g) For a volatility of 0.6, ..."
 
+
 END PROGRAM statistical_arbitrage
+
+!================================================================
+! (a) Derive the present value of the portfolio given in Eq. (2)
+!================================================================
+REAL, FUNCTION derive_present_value()
+  ! Parameters
+  INTEGER, PARAMETER :: t_invest_bond = 100  ! Time sell for risk free bond
+  INTEGER :: t0 = 0, dt = 1  ! Final day, Integration time
+  REAL, PARAMETER :: r =   ! Risk free rate
+  REAL, PARAMETER :: k =   !
+
+  ! Helpers
+  INTEGER :: t
+  REAL, DIMENSION(t0:t_invest_bond) :: v, share_price, discounted_value
+  share_price(1) = 100  ! Share price at t = 0
+  t_invest_bond = 100
+
+  ! Portfolio discounted value as a function of time (Eq. 2)
+  IF (t <= t_invest_bond) THEN
+    v(t) = share_price(t) * exp(-r*t) - share_price(1)
+  ELSE
+    v(t) = share_price(1) * k
+  END IF
+
+  ! Equation 2:
+  discounted_value(t) =
+
+END FUNCTION
+
+!===========================================================================
+! (b) Function that generates a normally distributed random number with
+! mean zero and variance one from the Box-Muller transform
+!===========================================================================
+REAL FUNCTION rand_box_muller()
+  INTEGER :: seed1 = 1, seed2 = 2
+  REAL :: x1, x2, z1, z2
+  REAL :: pi = 3.1415926535897932384626433832795028841971693993751058209749445
+  ! Intrinsic uniform random number
+  CALL SRAND(seed1); x1 = RAND()
+  CALL SRAND(seed2); x2 = RAND()
+
+  ! Generate normally distributed numbers, according to Box-Muller transform
+  z1 = sqrt(-2.0 * ln(x1) * cos(2.0 * pi * x2))
+  z2 = sqrt(-2.0 * ln(x1)) * sin (2.0 * pi * x2)
+
+  ! Only return either z1 or z2
+  ! RETURN z1, z2
+  rand_box_muller = z1
+END FUNCTION
+! Perhaps visualize lots of random numbers drawn from this function for report
+
+
+!==============================================================================
+! (b) Produce a realisation of daily share price for an annual drift, mu = 0.16
+! , and volatility, sigma = 0.2. Assume 250 trading days in a year. Use this to
+! simulate daily share price for one year.
+!==============================================================================
+ SUBROUTINE simulated_share_prices(t0, tfinal)
+  ! Parameters
+  REAL, PARAMETER :: mu = 0.16  ! annual drift
+  REAL, PARAMETER :: sigma = 0.2  ! volatility
+  REAL, PARAMETER :: r = 0.04  ! risk-free rate
+  REAL, PARAMETER  :: k = 0.2  !
+  INTEGER, PARAMETER :: t_final = 250  ! trading days in a year
+  INTEGER, PARAMETER :: dt = 1  ! daily intervals
+  INTEGER, PARAMETER :: t0 = 0
+
+  ! Helpers
+  REAL, DIMENSION(t0:t_final) :: share_price
+  REAL simulated_share_prices(t0:tfinal)  ! Set as a vector
+  REAL :: dw =  ! Wiener process
+  REAL :: delta_share
+  INTEGER :: t,
+
+  share_price(t0) = 100  ! Starting share price at t=0
+  dw = ...  ! Wiener process
+
+  ! Simulate daily share price over one year
+  t = 1
+  DO WHILE (t <= t_final)
+    delta_share = (mu * dt + sigma * dw) * share_price(t)
+    t = t + dt
+    share_price(t) = share_price(t-1) + delta_share  ! Evaluate price tomorrow
+  END DO
+
+  ! share_price_after(1, years, realisations)
+
+  realisations = 1000
+  dt = realisations / t_final
+
+  t_final = 1 * 250  ! 1 year
+  t_final = 2 * 250  ! 2 years
+  t_final = 5 * 250  ! 5 years
+
+  ! Evaluate v from t=0 up to 20 years
+  years = 20
+  mean_v = SUM(v) / realisations
+  the_sum = 0
+  n = realisations
+
+  DO i=0, n
+    the_sum = the_sum (v(i) - mean_v) ** 2.
+  ENDDO
+
+  variance_v = 1.0/(n-1) * sum(x - mean_v) ** 2
+END SUBROUTINE
